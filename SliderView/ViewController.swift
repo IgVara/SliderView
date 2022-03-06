ced91da2bd922ee3898219fffd28177f3aa3af82
Igor Varakutin <ivarakutin@gmail.com>
2022-03-06 23:52:37 +0300

    SliderView

--- SliderView/ViewController.swift
+++ SliderView/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colourView: UIView!
+    
+    @IBOutlet var bSlider: UISlider! {
+        didSet {
+            bSlider.tintColor = UIColor.blue
+        }
+    }
+    @IBOutlet var rSlider: UISlider! {
+        didSet {
+            rSlider.tintColor = UIColor.red
+        }
+    }
+    @IBOutlet var gSlider: UISlider! {
+        didSet {
+            gSlider.tintColor = UIColor.green
+        }
+    }
+    
+    @IBOutlet var rLabel: UILabel!
+    @IBOutlet var gLabel: UILabel!
+    @IBOutlet var bLabel: UILabel!
+    
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        rLabel.text = String(format: "%.2f",rSlider.value)
+        gLabel.text = String(format: "%.2f",gSlider.value)
+        bLabel.text = String(format: "%.2f",bSlider.value)
+        
     }
 
-
+    func changeColour() {
+        colourView.backgroundColor = UIColor(displayP3Red: CGFloat(rSlider.value), green: CGFloat(gSlider.value), blue: CGFloat(bSlider.value), alpha: 1)
+    }
+    @IBAction func rgbSliderChanged() {
+        changeColour()
+    }
+    @IBAction func numberSliderChanged() {
+        rLabel.text = String(format: "%.2f",rSlider.value)
+        gLabel.text = String(format: "%.2f",gSlider.value)
+        bLabel.text = String(format: "%.2f",bSlider.value)
+    }
+    
 }
 
